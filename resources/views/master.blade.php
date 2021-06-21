c0635641fa6c48980a53d00931ff49c172953eca
caeta32 <agustincarrera123@gmail.com>
2021-06-21 19:52:30 -0300

    favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,8 +9,7 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}"/>
-    <!------ Include the above in your HEAD tag ---------->
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">    <!------ Include the above in your HEAD tag ---------->
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
 </head>
