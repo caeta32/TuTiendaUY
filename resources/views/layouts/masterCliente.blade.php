331aab0d298b15816572a03325f02f0387935641
caeta32 <agustincarrera123@gmail.com>
2021-06-21 19:49:12 -0300

    favicon agregado

--- resources/views/layouts/masterCliente.blade.php
+++ resources/views/layouts/masterCliente.blade.php
@@ -8,7 +8,7 @@ $mail = Session::get('usuario')['email'];
 <html style="height: 100%; background-color: #F8F9FA">
 <head>
     <title>TuTienda</title>
-    <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     {{-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> --}}
